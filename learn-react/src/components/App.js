119b3107aa24392d5baee1f00ea184dba30be7c4
Nata Vacheishvili <nvacheishvili@wpi.edu>
2018-11-12 21:08:44 -0500

    changed the variable to prevState instead of using this.state

--- learn-react/src/components/App.js
+++ learn-react/src/components/App.js
@@ -40,14 +40,16 @@ class App extends Component {
   }
 
   handleAddPlayer = (name) => {
-    this.setState({
-      players: [
-        ...this.state.players,
-        {
-        name: name,
-        score: 0,
-        id: this.prevPlayerId += 1
-      }]
+    this.setState(prevState=>{
+      return {
+        players: [
+          ...prevState.players,
+          {
+          name: name,
+          score: 0,
+          id: this.prevPlayerId += 1
+        }]
+    }
     });
   }
 
