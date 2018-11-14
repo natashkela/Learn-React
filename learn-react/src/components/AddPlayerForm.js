d1746a2f20223a3fc5c12154025f630f8e08eb56
Nata Vacheishvili <nvacheishvili@wpi.edu>
2018-11-13 21:28:32 -0500

    Introducing refs

--- learn-react/src/components/AddPlayerForm.js
+++ learn-react/src/components/AddPlayerForm.js
@@ -1,29 +1,19 @@
 import React,{Component} from 'react';
 
 class AddPlayerForm extends Component{
-  state = {
-    value: ''
-  };
-
-  handleValueChange = (e)=>{
-    this.setState({
-      value: e.target.value
-    });
-  }
+  playerInput = React.createRef();
 
   handleSubmit = (e) => {
     e.preventDefault();
     const {addPlayer} = this.props;
-    addPlayer(this.state.value);
-    this.setState({
-      value: ''
-    });
+    addPlayer(this.playerInput.current.value);
+    e.currentTarget.reset();
   }
 
   render(){
     return(
         <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.value} onChange={this.handleValueChange }placeholder="Enter a player's name" />
+          <input type="text" ref={this.playerInput}  placeholder="Enter a player's name" />
           <input type="submit" value="Add Player" />
         </form>
     )
