df0f9f62f3a69fc2d6308d0db5df098089051e38
Nata Vacheishvili <nvacheishvili@wpi.edu>
2018-11-20 18:48:36 -0500

    I created navlinks for another page

--- educational-website/src/components/Courses.js
+++ educational-website/src/components/Courses.js
@@ -1,18 +1,26 @@
 import React from 'react';
-
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
+import Course from './courses/Course'
+import Css from './courses/CSS'
+import Html from './courses/HTML'
+import JavaScript from './courses/JavaScript'
 const Courses = () => (
+  <BrowserRouter>
   <div className="main-content courses">
     <div className="course-header group">
-      <h2>Courses</h2> 
+      <h2>Courses</h2>
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to='/courses/html'>HTML</NavLink></li>
+        <li><NavLink to='/courses/css'>CSS</NavLink></li>
+        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
-    
-    {/* Write routes here... */}
+
+    <Route path="/courses/html" component={Html} />
+    <Route path="/courses/css" component={Css} />
+    <Route path="/courses/javascript" component={JavaScript} />
   </div>
+  </BrowserRouter>
 );
 
 export default Courses;
