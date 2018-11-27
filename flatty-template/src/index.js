9cf07339546d3eae5ce311da431244ed87fd25fc
Nata Vacheishvili <nvacheishvili@wpi.edu>
2018-11-26 21:47:13 -0500

    Starting to convert bootstrap template into react

---
+++ flatty-template/src/index.js
@@ -0,0 +1,9 @@
+import './css/bootstrap.css';
+import './css/main.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
