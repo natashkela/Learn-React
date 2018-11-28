d4259efa075f2daaa51a58337b0a5a355f2145ef
Nata Vacheishvili <nvacheishvili@wpi.edu>
2018-11-27 19:50:52 -0500

    Done

--- flatty-template/src/components/Feature.js
+++ flatty-template/src/components/Feature.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import feature1 from '../img/ser01.png';
-import feature2 from '../img/ser02.png';
-import feature3 from '../img/ser03.png';
 const Feature = ({img, featureHeader, featureText, counter}) =>{
   return (
     <div className="col-lg-4">
-      <img src={feature1} width="180" alt="" />
+      <img src={require('../img/ser0'+counter+'.png')} width="180" alt="" />
       <h4>{counter} - {featureHeader}</h4>
       <p>{featureText}</p>
 
--- flatty-template/src/components/TeamMember.js
+++ flatty-template/src/components/TeamMember.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import pic1 from '../img/pic1.jpg';
-import pic2 from '../img/pic2.jpg';
-import pic3 from '../img/pic3.jpg';
 const TeamMember = ({name,about,index}) => {
   return (
     <div className="col-lg-4">
-      <img className="img-circle" src={pic1} width="140" alt="" />
+      <img className="img-circle" src={require('../img/pic'+index+'.jpg')} width="140" alt="" />
       <h4>{name}</h4>
       <p>{about}</p>
       <p><i className="glyphicon glyphicon-send"></i> <i className="glyphicon glyphicon-phone"></i> <i className="glyphicon glyphicon-globe"></i></p>
