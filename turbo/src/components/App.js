e72a58463a49f967e7903b9d1790fbe1337fc12a
Nata Vacheishvili <nvacheishvili@wpi.edu>
2018-12-02 11:32:07 -0500

    Our Mission Component Added

--- turbo/src/components/App.js
+++ turbo/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar.js';
 import Footer from './Footer.js';
+import OurMission from './OurMission.js';
 class App extends Component {
   render() {
     return (
@@ -98,28 +99,7 @@ class App extends Component {
               <div className="row">
                 <div className="col-md-11">
                   <div className="rq-mission-block">
-                    <div className="row">
-                      <div className="col-md-6">
-                        <div className="mission-content">
-                          <h1 className="rq-title">Our Mission<span className="rq-dot">.</span><i className="rq-line"></i></h1>
-                          <p className="mission-text">Suspendisse hendrerit facilisis ante. Sed feugiat lectus
-                          non tim porttitor aliquam. Ut a ipsum tempus, venenatis nibh et, eleifefend nisi.
-                          Etiam hendrerit, purus ac varius vestibulum, enim lon arcu euismod purus,
-                          id hendrerit tellus turpis biben dum exilon Morbi cursus justo ut malesuada tempus.
-                          </p>
-                          <address>
-                            <a href="#">Brasion Mike</a>
-                            <cite>- CEO Founder</cite>
-                          </address>
-                        </div>
-                      </div>
-                      <div className="col-md-6">
-                        <div className="mission-image">
-                          <img src="img/block-transparent-img.png" alt="" />
-                        </div>
-                      </div>
-                    </div>
-
+                    <OurMission />
                   </div>
                 </div>
               </div>
