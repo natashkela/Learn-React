309570cdfbb972f5a2e52ad5a648b11adb669767
Nata Vacheishvili <nvacheishvili@wpi.edu>
2018-12-03 20:13:26 -0500

    Everything should be all set now

--- turbo/src/components/TabAvailableCarsBrief.js
+++ turbo/src/components/TabAvailableCarsBrief.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TabAvailableCarsLong from './TabAvailableCarsLong.js';
-const TabAvailableCarsBrief = ({list}) => {
+const TabAvailableCarsBrief = ({list,id}) => {
   return (
-    <div className="child-tab-wrapper">
-      <ul className="nav nav-tabs" role="tablist">
-        {list.map((car, index)=>
-          <li role="presentation" key={index} className={index == 0 ? "active" : ""}>
-            <a href={"#car-"+(index+1)}  role="tab" data-toggle="tab">
-              <img src={require("../img/tab-image/thumbnail"+(index+1)+".jpg")} alt="" />
-              <span className="tittle">{car.title}</span>
-              <span className="car-des">{car.description}</span>
-              <span className="rent-price">${car.price}<b>/Day</b></span>
-            </a>
-          </li>
-        )}
-      </ul>
-      <TabAvailableCarsLong list={list}/>
+    <div role="tabpanel" class="tab-pane fade in active" id={id}>
+      <div className="child-tab-wrapper">
+        <ul className="nav nav-tabs" role="tablist">
+          {list.map((car, index)=>
+            <li role="presentation" key={index} className={index == 0 ? "active" : ""}>
+              <a href={"#car-"+(index+1)}  role="tab" data-toggle="tab">
+                <img src={require("../img/tab-image/thumbnail"+(index+1)+".jpg")} alt="" />
+                <span className="tittle">{car.title}</span>
+                <span className="car-des">{car.description}</span>
+                <span className="rent-price">${car.price}<b>/Day</b></span>
+              </a>
+            </li>
+          )}
+        </ul>
+        <TabAvailableCarsLong list={list}/>
+      </div>
     </div>
   );
 }
