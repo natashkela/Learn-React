e0cbd8cd02c3f3e41e6cdc83155a4a1885fdf4d2
Nata Vacheishvili <nvacheishvili@wpi.edu>
2018-12-18 21:07:16 -0500

    fully dynamic so far

--- turbo/src/components/BlogPostDescription.js
+++ turbo/src/components/BlogPostDescription.js
@@ -4,17 +4,17 @@ const BlogPostDescription = ({information}) => {
   return (
     <div className="rq-listing-single">
       <div className="rq-listing-standard-image-post">
-        <img src="img/blog-listing/listing-img.png" alt="" />
-        <h3 className="rq-listing-title"><a href="#">{information.title}</a></h3>
+        <img src={require("../img/blog-listing/listing-img.png")} alt="" />
+        <h3 className="rq-listing-title"><a href={"/blog-post?id="+information.blogId}>{information.title}</a></h3>
         <div className="rq-listing-meta">
-          <span className="author-name"><a href="#">{information.author}</a></span>
+          <span className="author-name">{information.author}</span>
           <span className="v-line">|</span>
-          <span className="date"><a href="#">{information.date}</a></span>
+          <span className="date">{information.date}</span>
         </div>
         <p className="post-content">
           {information.shortDescription}
         </p>
-        <a className="continue-btn rq-btn rq-btn-normal" href="#">Continue <i className="arrow_right"></i></a>
+        <a className="continue-btn rq-btn rq-btn-normal" href={"/blog-post?id="+information.blogId}>Continue <i className="arrow_right"></i></a>
       </div>
     </div>
   );
