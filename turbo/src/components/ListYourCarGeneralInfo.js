825113294fb28c7279eb510d9e02b46ce08e760f
Nata Vacheishvili <nvacheishvili@wpi.edu>
2019-01-03 18:15:39 -0500

    general-info finished

--- turbo/src/components/ListYourCarGeneralInfo.js
+++ turbo/src/components/ListYourCarGeneralInfo.js
@@ -15,13 +15,13 @@ class ListYourCarGeneralInfo extends Component {
   }
   render(){
     return (
-      <div className="list-your-car-general-info">
+      <div className="list-your-car-general-info margin-bottom-40">
         <h5 className="margin-top-10 margin-bottom-0"><strong>Where is your car located?</strong></h5>
         <div className="row">
-          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 country">
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 select-transparent">
             <CountryListSelect selected={this.state.carLocationCountry} selectCountry={this.handleSelectCountry.bind(this)}/>
           </div>
-          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 city">
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 select-transparent">
             <CityListSelect countrySelected={this.state.carLocationCountry} citySelected={this.state.carLocationCity} selectRegion={this.handleSelectCity.bind(this)} />
           </div>
           <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
@@ -30,8 +30,28 @@ class ListYourCarGeneralInfo extends Component {
           <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
             <input type="text" className="rq-form-control" placeholder="Zip Code" />
           </div>
-          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <Map />
+        </div>
+        <h5 className="margin-top-25 margin-bottom-0"><strong>Car Information</strong></h5>
+        <div className="row">
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 select-transparent">
+            <select className="rq-form-control reverse">
+              <option>Car Make</option>
+            </select>
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 select-transparent">
+            <select className="rq-form-control reverse">
+              <option>Car Year</option>
+            </select>
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 select-transparent">
+            <select className="rq-form-control reverse">
+              <option>Car Name</option>
+            </select>
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 select-transparent margin-bottom-40">
+            <select className="rq-form-control reverse">
+              <option>Car Trim</option>
+            </select>
           </div>
         </div>
       </div>
