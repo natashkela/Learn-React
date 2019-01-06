43d6fcbe5db6594f85d0f9fbc4fb6ec8dd3f32f8
Nata Vacheishvili <nvacheishvili@wpi.edu>
2019-01-05 20:05:51 -0500

    List Your Car Design finished

--- turbo/src/components/ListYourCarAvailability.js
+++ turbo/src/components/ListYourCarAvailability.js
@@ -3,10 +3,10 @@ const ListYourCarAvailability = () => {
   return (
     <div className="list-your-car-availability margin-bottom-40">
       <h5 className="margin-top-10 margin-bottom-0"><strong>Car Availability</strong></h5>
-      <div className="row margin-top-30 ">
+      <div className="row margin-top-5">
         <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 select-transparent">
           <span className="more-info">How much advance notice do you need to confirm a trip? </span>
-          <select className="rq-form-control reverse">
+          <select className="margin-top-5 rq-form-control reverse">
             <option>Advance Notice</option>
           </select>
         </div>
@@ -15,10 +15,10 @@ const ListYourCarAvailability = () => {
           <p>Blocks trips that don't give you enough advance notice</p>
         </div>
       </div>
-      <div className="row margin-top-30 ">
+      <div className="row margin-top-15 ">
         <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 select-transparent">
         <span className="more-info">How long would you like trips to last? </span>
-          <select className="rq-form-control reverse">
+          <select className="margin-top-5 rq-form-control reverse">
             <option>Minimum Duration</option>
           </select>
         </div>
@@ -27,7 +27,7 @@ const ListYourCarAvailability = () => {
           <p>Shortest possible amount of time people should rent your car</p>
         </div>
       </div>
-      <div className="row margin-top-30 ">
+      <div className="row margin-top-15 ">
         <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 select-transparent">
           <select className="rq-form-control reverse">
             <option>Maximum Duration</option>
--- turbo/src/components/ListYourCarContent.js
+++ turbo/src/components/ListYourCarContent.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import ListYourCarGeneralInfo from './ListYourCarGeneralInfo';
 import ListYourCarDetails from './ListYourCarDetails';
 import ListYourCarAvailability from './ListYourCarAvailability';
+import ListYourCarPhotos from './ListYourCarPhotos';
 const ListYourCarContent = ({tab}) => {
   return (
     <div>
@@ -14,6 +15,9 @@ const ListYourCarContent = ({tab}) => {
       }
       {tab.uniqueName == "car-availability" &&
         <ListYourCarAvailability />
+      }
+      {tab.uniqueName == "car-photos" &&
+        <ListYourCarPhotos />
       }
     </div>
   );
