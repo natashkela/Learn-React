701e6db41f604de987449020dbfe175caf492d61
Nata Vacheishvili <nvacheishvili@wpi.edu>
2019-08-21 18:23:52 -0400

    start fresh after assignment

--- my-react-app/src/Person/Person.js
+++ my-react-app/src/Person/Person.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 import './Person.css';
-const Person = (props) => {
+
+const person = ( props ) => {
     return (
         <div className="Person">
-            <p onClick={props.click}>
-                Me var {props.name} da var {props.age} wlis
-            </p>
-            <p onClick={props.click}>
-                {props.children}
-            </p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
         </div>
-    );
+    )
 };
-Person.propTypes = {
-    name: PropTypes.string,
-    age: PropTypes.number,
-    click: PropTypes.func,
-    changed: PropTypes.func
-};
-export default Person;
+
+export default person;
